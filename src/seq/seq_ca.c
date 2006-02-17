39f404a1b4b7212fd99097b2276739290d4c07da
dayle <dayle@slac.stanford.edu>
2006-02-17 00:44:15 +0000

    changes to specify CROSS_COMPILER_TARGET_ARCHS = RTEMS-beatnik (and linux-x86 where needed). There is no build for uC5282 in site

--- src/seq/seq_ca.c
+++ src/seq/seq_ca.c
@@ -227,9 +227,10 @@ LOCAL void proc_db_events(pvValue *pValue, pvType type, CHAN *pDB,
 	/* Copy error message (only when severity indicates error) */
 	if (pDB->severity != pvSevrNONE)
 	{
-	    char *pmsg = pvVarGetMess(pDB->pvid);
-	    if (!pmsg) pmsg = "unknown";
-	    pDB->message = Strdcpy(pDB->message, pmsg);
+            if (pvVarGetMess(pDB->pvid))
+                pDB->message = Strdcpy(pDB->message, pvVarGetMess(pDB->pvid));
+            else
+                pDB->message = Strdcpy(pDB->message, "unknown");
 	}
 
 	/* Get ptr to the state program that owns this db entry */
